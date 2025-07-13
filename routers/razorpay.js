924b61b3ca493e7e730487907b753bf9bf8b1eae
Devashish Rathod <rathod.devashish09@gmail.com>
2025-07-14 02:10:39 +0530

    add token validation for subscribed any plan

--- routers/razorpay.js
+++ routers/razorpay.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
+const { verifyToken, checkRole } = require("../middleware/authValidation");
 const {
   createOrder,
   verifyTransaction,
 } = require("../controller/transactions");
+const { ROLES } = require("../constants");
 
-router.post("/create-order", createOrder);
-router.put("/verifyPayment", verifyTransaction);
+router.post(
+  "/create-order",
+  verifyToken,
+  checkRole(ROLES.ADMIN, ROLES.VENDOR),
+  createOrder
+);
+router.put(
+  "/verifyPayment",
+  verifyToken,
+  checkRole(ROLES.ADMIN, ROLES.VENDOR),
+  verifyTransaction
+);
 
 module.exports = router;
