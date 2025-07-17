f60f9a276c70db224fee48712c68e69c10007a03
Devashish Rathod <rathod.devashish09@gmail.com>
2025-07-17 18:49:56 +0530

    populate data of brand and subBrand

--- service/brandServices/getBrandWithAllDetails.js
+++ service/brandServices/getBrandWithAllDetails.js
@@ -1,14 +1,15 @@
 const Brand = require("../../model/Brand");
 
 exports.getBrandWithAllDetails = async (id) => {
-  return await Brand.findOne({ _id: id, isDeleted: false })
-    .populate("user")
-    .populate("category")
-    .populate("subCategory")
-    .populate("location")
-    .populate("gst")
-    .populate("workHours")
-    .populate("bankAccount")
-    .populate("transaction")
-    .populate("subscribed");
+  return await Brand.findOne({ _id: id, isDeleted: false }).populate(
+    "user category subCategory gst location workHours bankAccount transaction subscribed"
+  );
+  // .populate("category")
+  // .populate("subCategory")
+  // .populate("location")
+  // .populate("gst")
+  // .populate("workHours")
+  // .populate("bankAccount")
+  // .populate("transaction")
+  // .populate("subscribed");
 };
--- service/subBrandServices/getSubBrandWithAllDetails.js
+++ service/subBrandServices/getSubBrandWithAllDetails.js
@@ -1,15 +1,7 @@
 const SubBrand = require("../../model/SubBrand");
 
 exports.getSubBrandWithAllDetails = async (id) => {
-  return await SubBrand.findOne({ _id: id, isDeleted: false })
-    .populate("user")
-    .populate("location")
-    .populate("workHours")
-    .populate("brand");
-  // .populate("category")
-  // .populate("subCategory")
-  // .populate("gst")
-  // .populate("bankAccount")
-  // .populate("transaction")
-  // .populate("subscribed");
+  return await SubBrand.findOne({ _id: id, isDeleted: false }).populate(
+    "user brand location workHours"
+  );
 };
