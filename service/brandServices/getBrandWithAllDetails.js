761514d138ac9012e08f9656593da58970cc88e0
Devashish Rathod <rathod.devashish09@gmail.com>
2025-07-17 16:57:25 +0530

    remove subBrand from brand populate

--- service/brandServices/getBrandWithAllDetails.js
+++ service/brandServices/getBrandWithAllDetails.js
@@ -3,7 +3,6 @@ const Brand = require("../../model/Brand");
 exports.getBrandWithAllDetails = async (id) => {
   return await Brand.findOne({ _id: id, isDeleted: false })
     .populate("user")
-    .populate("subBrand")
     .populate("category")
     .populate("subCategory")
     .populate("location")
