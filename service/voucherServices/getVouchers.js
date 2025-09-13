a942e3294836a79690b68090c63b884dc13285a2
Devashish Rathod <rathod.devashish09@gmail.com>
2025-09-13 16:24:41 +0530

    get all vouchers with report done

--- service/voucherServices/getVouchers.js
+++ service/voucherServices/getVouchers.js
@@ -1,7 +1,7 @@
 // const mongoose = require("mongoose");
 // const Voucher = require("../../model/Voucher");
 
-// exports.getVouchers = async (query) => {
+// exports.getVouchers = async (userId, query) => {
 //   const {
 //     brandId,
 //     subBrandId,
@@ -16,19 +16,20 @@
 //     limit = 10,
 //     sortBy = "createdAt",
 //     sortOrder = "desc",
+//     onlyLiked = false,
 //   } = query;
 //   const filter = { isDeleted: false };
 //   if (brandId && mongoose.isValidObjectId(brandId)) {
-//     filter.brand = new mongoose.Types.ObjectId(brandId.toString());
+//     filter.brand = new mongoose.Types.ObjectId(brandId);
 //   }
 //   if (subBrandId && mongoose.isValidObjectId(subBrandId)) {
-//     filter.subBrands = new mongoose.Types.ObjectId(subBrandId.toString());
+//     filter.subBrands = new mongoose.Types.ObjectId(subBrandId);
 //   }
 //   if (categoryId && mongoose.isValidObjectId(categoryId)) {
-//     filter.category = new mongoose.Types.ObjectId(categoryId.toString());
+//     filter.category = new mongoose.Types.ObjectId(categoryId);
 //   }
 //   if (subCategoryId && mongoose.isValidObjectId(subCategoryId)) {
-//     filter.subCategory = new mongoose.Types.ObjectId(subCategoryId.toString());
+//     filter.subCategory = new mongoose.Types.ObjectId(subCategoryId);
 //   }
 //   if (status) filter.status = status;
 //   if (isPublished !== undefined) filter.isPublished = isPublished === "true";
@@ -38,27 +39,82 @@
 //     if (validFrom) filter.validFrom.$gte = new Date(validFrom);
 //     if (validTill) filter.validFrom.$lte = new Date(validTill);
 //   }
-//   const sortOption = {
-//     [sortBy]: sortOrder === "asc" ? 1 : -1,
-//   };
+//   const sortOption = { [sortBy]: sortOrder === "asc" ? 1 : -1 };
 //   const skip = (parseInt(page) - 1) * parseInt(limit);
-//   const [vouchers, total] = await Promise.all([
-//     Voucher.find(filter)
-//       .populate({
-//         path: "brand",
-//         populate: { path: "location" },
-//       })
-//       .populate("subBrands")
-//       .sort(sortOption)
-//       .skip(skip)
-//       .limit(parseInt(limit)),
-//     Voucher.countDocuments(filter),
-//   ]);
+//   const pipeline = [
+//     { $match: filter },
+//     {
+//       $lookup: {
+//         from: "favoritevouchers",
+//         let: { voucherId: "$_id" },
+//         pipeline: [
+//           {
+//             $match: {
+//               $expr: {
+//                 $and: [
+//                   { $eq: ["$voucher", "$$voucherId"] },
+//                   { $eq: ["$user", new mongoose.Types.ObjectId(userId)] },
+//                   { $eq: ["$isDeleted", false] },
+//                 ],
+//               },
+//             },
+//           },
+//         ],
+//         as: "userLiked",
+//       },
+//     },
+//     {
+//       $addFields: {
+//         isLiked: {
+//           $cond: [{ $gt: [{ $size: "$userLiked" }, 0] }, true, false],
+//         },
+//       },
+//     },
+//     ...(onlyLiked === "true" ? [{ $match: { isLiked: true } }] : []),
+//     {
+//       $lookup: {
+//         from: "brands",
+//         localField: "brand",
+//         foreignField: "_id",
+//         as: "brand",
+//       },
+//     },
+//     { $unwind: { path: "$brand", preserveNullAndEmptyArrays: true } },
+//     {
+//       $lookup: {
+//         from: "locations",
+//         localField: "brand.location",
+//         foreignField: "_id",
+//         as: "brand.location",
+//       },
+//     },
+//     {
+//       $lookup: {
+//         from: "subbrands",
+//         localField: "subBrands",
+//         foreignField: "_id",
+//         as: "subBrands",
+//       },
+//     },
+//     { $sort: sortOption },
+//     {
+//       $facet: {
+//         vouchers: [{ $skip: skip }, { $limit: parseInt(limit) }],
+//         totalCount: [{ $count: "count" }],
+//       },
+//     },
+//     {
+//       $addFields: {
+//         total: { $ifNull: [{ $arrayElemAt: ["$totalCount.count", 0] }, 0] },
+//       },
+//     },
+//   ];
+//   const result = await Voucher.aggregate(pipeline);
 //   return {
-//     total,
+//     total: result[0]?.total || 0,
 //     page: parseInt(page),
 //     limit: parseInt(limit),
-//     vouchers,
+//     vouchers: result[0]?.vouchers || [],
 //   };
 // };
 
@@ -155,9 +211,149 @@ exports.getVouchers = async (userId, query) => {
     {
       $lookup: {
         from: "subbrands",
-        localField: "subBrands",
-        foreignField: "_id",
+        let: { subBrandIds: "$subBrands" },
+        pipeline: [
+          { $match: { $expr: { $in: ["$_id", "$$subBrandIds"] } } },
+          {
+            $lookup: {
+              from: "locations",
+              localField: "location",
+              foreignField: "_id",
+              as: "location",
+            },
+          },
+          {
+            $lookup: {
+              from: "bills",
+              let: { sbId: "$_id" },
+              pipeline: [
+                { $match: { $expr: { $eq: ["$subBrandId", "$$sbId"] } } },
+                {
+                  $group: {
+                    _id: null,
+                    billValues: { $sum: "$billAmount" },
+                    earnValues: { $sum: "$finalPayable" },
+                  },
+                },
+              ],
+              as: "billStats",
+            },
+          },
+          {
+            $addFields: {
+              billValues: {
+                $ifNull: [{ $arrayElemAt: ["$billStats.billValues", 0] }, 0],
+              },
+            },
+          },
+          {
+            $addFields: {
+              earnValues: {
+                $ifNull: [{ $arrayElemAt: ["$billStats.earnValues", 0] }, 0],
+              },
+            },
+          },
+          {
+            $lookup: {
+              from: "refunds",
+              let: { sbId: "$_id" },
+              pipeline: [
+                {
+                  $match: {
+                    $expr: {
+                      $and: [
+                        { $eq: ["$subBrand", "$$sbId"] },
+                        { $eq: ["$status", "APPROVED"] },
+                        { $eq: ["$isDeleted", false] },
+                      ],
+                    },
+                  },
+                },
+                {
+                  $group: {
+                    _id: null,
+                    refundValues: { $sum: "$refundAmount" },
+                  },
+                },
+              ],
+              as: "refundStats",
+            },
+          },
+          {
+            $addFields: {
+              refundValues: {
+                $ifNull: [
+                  { $arrayElemAt: ["$refundStats.refundValues", 0] },
+                  0,
+                ],
+              },
+            },
+          },
+        ],
         as: "subBrands",
+      },
+    },
+    {
+      $lookup: {
+        from: "bills",
+        let: { vId: "$_id" },
+        pipeline: [
+          { $match: { $expr: { $eq: ["$voucherId", "$$vId"] } } },
+          {
+            $group: {
+              _id: null,
+              invoices: { $sum: 1 },
+              billValues: { $sum: "$billAmount" },
+              earnValues: { $sum: "$finalPayable" },
+            },
+          },
+        ],
+        as: "voucherBillStats",
+      },
+    },
+    {
+      $addFields: {
+        invoices: {
+          $ifNull: [{ $arrayElemAt: ["$voucherBillStats.invoices", 0] }, 0],
+        },
+        billValues: {
+          $ifNull: [{ $arrayElemAt: ["$voucherBillStats.billValues", 0] }, 0],
+        },
+        earnValues: {
+          $ifNull: [{ $arrayElemAt: ["$voucherBillStats.earnValues", 0] }, 0],
+        },
+        outlets: { $size: "$subBrands" },
+      },
+    },
+    {
+      $lookup: {
+        from: "refunds",
+        let: { vId: "$_id" },
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $and: [
+                  { $eq: ["$voucher", "$$vId"] },
+                  { $eq: ["$status", "APPROVED"] },
+                  { $eq: ["$isDeleted", false] },
+                ],
+              },
+            },
+          },
+          { $group: { _id: null, refundValues: { $sum: "$refundAmount" } } },
+        ],
+        as: "voucherRefundStats",
+      },
+    },
+    {
+      $addFields: {
+        refundValues: {
+          $ifNull: [
+            { $arrayElemAt: ["$voucherRefundStats.refundValues", 0] },
+            0,
+          ],
+        },
       },
     },
     { $sort: sortOption },
